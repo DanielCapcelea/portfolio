b0da58317b8ab84f6464a36cdb44624838755559
Daniel <danielcapcelea@mail.ru>
2022-04-25 22:54:06 +0200

    wip

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { NavigationDots, SocialMedia } from '../components';
+import {NavigationDots} from '../components';
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
     return (
         <div id={idName} className={`app__container ${classNames}`}>
-            <SocialMedia />
             <div className="app__wrapper app__flex">
-                <Component />
-
-                <div className="copyright">
-                    <p className="p-text">@2020 DANIEL</p>
-                    <p className="p-text">All rights reserved</p>
-                </div>
+                <Component/>
             </div>
-            <NavigationDots active={idName} />
+            <NavigationDots active={idName}/>
         </div>
     );
 };
